9c76a1d9f0d2ebdcbd2bce09b780047a458d090d
Minecrell <dev@minecrell.net>
2014-10-04 16:56:37 +0200

    Try to fix website deploying.

--- gulpfile.js
+++ gulpfile.js
@@ -179,7 +179,7 @@ gulp.task('travis-deploy', ['build:prod'], function () {
   return gulp.src(['./public/**/*', './public/.travis.yml'])
     .pipe(deploy({
       branch: 'master',
-      remoteUrl: "https://" + process.env.GH_TOKEN + "@" + process.env.GH_REF
+      remoteUrl: process.env.GIT_REPO
     }));
 
 });
